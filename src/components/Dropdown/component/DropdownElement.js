956ed761c91b9345704bdd21a4e08c9f3531199b
mattsetyadi <matt.musicianstuff@gmail.com>
2021-03-31 02:07:53 +0700

    Filter feature
    We can filter on dropdown menu and add onTouvhEnd event if obile device can't recognize touch for toggle

--- src/components/Dropdown/component/DropdownElement.js
+++ src/components/Dropdown/component/DropdownElement.js
@@ -2,40 +2,79 @@ import React, { useState, useRef, useEffect } from 'react';
 
 const DropdownElement = ({ options, prompt, value, onChange, id, label }) => {
   const [open, setOpen] = useState(false);
+  const [query, setQuery] = useState('');
   const ref = useRef(null);
 
   useEffect(() => {
-    document.addEventListener('click', close);
+    // Special case if we cannot touch on mobile phone
+    ['click', 'touchend'].forEach((e) => {
+      document.addEventListener('click', toggle);
+    });
+    // normal use if mobile touch worked
+    // document.addEventListener('click', toggle);
     return () => {
-      document.removeEventListener('click', close);
+      // Special case too on component unmount
+      ['click', 'touchend'].forEach((e) => {
+        document.removeEventListener('click', toggle);
+      });
+      //   normal use if mobile touch worked
+      //   document.removeEventListener('click', toggle);
     };
   }, []);
 
-  function close(e) {
+  const toggle = (e) => {
     // console.dir(e.target, e.current);
     // console.dir('e target >>', e.target);
     // console.dir('e current >>', e.current);
     // for onBlur
     setOpen(e && e.target === ref.current);
-  }
+  };
+
+  const selectOption = (option) => {
+    setQuery('');
+    onChange(option);
+    setOpen(false);
+  };
+
+  const filter = (options) => {
+    return options.filter(
+      (option) => option[label].toLowerCase().indexOf(query.toLowerCase()) > -1,
+    );
+  };
+
+  const displayValue = () => {
+    if (query.length > 0) return query;
+    if (value) return value[label];
+    return '';
+  };
 
   return (
     <div className='dropdown'>
-      <div className='control' onClick={() => setOpen((prev) => !prev)}>
-        <div className='selected-value' ref={ref}>
-          {value ? value[label] : prompt}
+      <div className='control'>
+        <div className='selected-value'>
+          <input
+            type='text'
+            ref={ref}
+            placeholder={value ? value[label] : prompt}
+            value={displayValue()}
+            onChange={(e) => {
+              setQuery(e.target.value);
+              // dropdown is already have a value
+              // so we have to unselect the value and start filter the value
+              onChange(null);
+            }}
+            onClick={toggle}
+            onTouchEnd={toggle}
+          />
         </div>
         <div className={`arrow ${open ? 'open' : null}`} />
       </div>
       <div className={`options ${open ? 'open' : null}`}>
-        {options.map((option) => (
+        {filter(options).map((option) => (
           <div
             key={option[id]}
             className={`option ${value === option ? 'selected' : null}`}
-            onClick={() => {
-              onChange(option);
-              setOpen(false);
-            }}
+            onClick={() => selectOption(option)}
           >
             {option[label]}
           </div>
